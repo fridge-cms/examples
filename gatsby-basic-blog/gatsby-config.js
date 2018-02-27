637e6cfcd91b5669662159e215376509a4ca2743
Mike Kruk <tamagokun@gmail.com>
2018-02-27 12:42:48 -0500

    super basic gatsby example

---
+++ gatsby-basic-blog/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-fridge',
+      options: {
+        token: '0GTdA28HZXZXLNfOKHUOz'
+      }
+    }
+  ]
+}
---
+++ gatsby-basic-blog/src/pages/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default ({data}) =>
+  <main>
+    <header>
+      <h1>{data.fridgeHomepage.title}</h1>
+      <p>{data.fridgeHomepage.bio}</p>
+    </header>
+    <div>
+      {data.allFridgePosts.edges.map(({node: post}) =>
+        <article>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+        </article>
+      )}
+    </div>
+  </main>
+
+
+export const query = graphql`
+query indexQuery {
+  fridgeHomepage {
+    title
+    bio
+  }
+
+  allFridgePosts {
+    edges {
+      node {
+        slug
+        title
+        description
+        date_created
+      }
+    }
+  }
+}
+`
